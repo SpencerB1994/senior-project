49da1c9780a7fd0882c5f1be5aa07ac71d62f1f7
Spencer Butterfield <115520984+SpencerB1994@users.noreply.github.com>
2023-06-26 17:00:25 -0700

    added some form validation js to the contact page

--- javascript.js
+++ javascript.js
@@ -8,6 +8,21 @@ function myFunction() {
   }
 }
 
+/* Plan Trip Page Form - Random Eligibility */
+function randomWinner(event) {
+  let randomCountry = document.getElementsByTagName("Option");
+  let eli = document.getElementsByClassName("eligibility");
+
+  for (i = 0; i < arr.length; i++) {
+    if (randomCountry === 'United States' || randomCountry === 'Canada') {
+      eli.push('You are eligible for a direct flight!');
+      eli.classList.add("error");
+    } else {
+      eli.push('You are not eligible for a direct flight.');
+    }
+  }
+}
+
 /* Accordion Action on FAQ Page*/
 var acc = document.getElementsByClassName("accordion");
 var i;
@@ -27,3 +42,54 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+/* Contact form validation */
+function validateForm(event) {
+  const formErrors = document.getElementById("formErrors");
+  const fName = document.getElementById("fName");
+  const lName = document.getElementById("lName");
+  const email = document.getElementById("emailInput");
+  const messageInput = document.getElementById("messageBox");
+  const errors = [];
+
+  formErrors.classList.add("hide");
+  fName.classList.remove("error");
+  lName.classList.remove("error");
+  email.classList.remove("error");
+
+  if (!fName.value.length < 1 || !fName.value !== "") {
+    errors.push("Missing first name.");
+    fName.classList.add("error");
+  }
+
+  if (!lName.value.length < 1 || !lName.value !== "") {
+    errors.push("Missing last name.");
+    lName.classList.add("error");
+  }
+
+  const regexEmail = /[A-Za-z]+@[A-Za-z]+\.com/i;
+  if (!email.value.length < 1 || !email.value !== regexEmail) {
+    errors.push("Must provide a valid email address.");
+    email.classList.add("error");
+  }
+
+  if (!messageInput.value.length < 1 || !messageInput.value !== "") {
+    errors.push("Must provide a message.");
+    messageInput.classList.add("error");
+  }
+
+  if (errors.length > 0) {
+    formErrors.classList.remove("hide");
+    const errorList = document.querySelector("#formErrors");
+    errors.forEach((error) => {
+      const li = document.createElement("li");
+      li.innerHTML = error;
+      formErrors.appendChild(li);
+    });
+  }
+}
+
+document.getElementById("submit").addEventListener("click", function(event) {
+  validateForm();
+  event.preventDefault();
+});
